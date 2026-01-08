c8893da18b872eeae641bccaca65863ac8b62c34
Iker 275 <jeananderson275@gmail.com>
2026-01-08 18:11:17 +1000

    using set interval to show counter clock and state

--- src/components/HookPractise.jsx
+++ src/components/HookPractise.jsx
@@ -8,15 +8,17 @@ let time = new Date().toLocaleTimeString();
 
     var [counter, setCounter] = useState(time);
 
-console.log(time);
 
+    function realTime() {
+        setInterval(getTime, 1000);
+    }
     function getTime() {
         let time = new Date().toLocaleTimeString();
         setCounter(time);
     }
     return <div className="container">
         <h1>{counter}</h1>
-        <button onClick={getTime}>GET TIME</button>
+        <button onClick={realTime}>GET TIME</button>
 
     </div>;
 }
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ root.render(
   
     {/* <App /> */}
      {/* REACT STATESTATE EXAMPLE */}
-    <HookApp />
+    <HookPractise />
   </React.StrictMode>
 );
 
